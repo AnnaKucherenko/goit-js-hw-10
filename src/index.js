24324c0a1bff0498a977a348c945df891cabd8e4
AnnaKucherenko <annakucherenko31@gmail.com>
2022-02-11 13:35:38 +0200

    список стран

--- src/index.js
+++ src/index.js
@@ -1,42 +1,88 @@
-import throttle from 'lodash.throttle';
+import debounce from 'lodash/debounce';
 import Notiflix from 'notiflix';
 import './css/styles.css';
 import  { fetchCountries }  from './api/fetchCountries';
 import countryCard from './tamplates/country-card.hbs';
 
 
-
 const DEBOUNCE_DELAY = 300;
 
+
 const inputEl = document.querySelector('#search-box');
+const countryList =  document.querySelector('.country-list');
 const cardContainer =  document.querySelector('.country-info');
-inputEl.addEventListener('input', throttle (onTextInput, DEBOUNCE_DELAY));
+inputEl.addEventListener('input', onTextInput);
 
 function onTextInput(evt){
-    evt.preventDefault();
+  evt.preventDefault();
   const input = evt.currentTarget.value;
   console.log(input);
-    const trimmedString=input.trim();
-    
-    console.log(trimmedString);
-    
+  
+  if (input.length === 1) {
+     Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+  }
+  const trimmedString=input.trim();
+   
   fetchCountries(trimmedString)
-    // .then(renderCountryList)
     .then(renderCountryCard)
     .catch(onFetchError);
     // .finally(() => input.reset());
 }
 
-// function renderCountryList(name) {
-  
-// }
+// способ как выбрать отрисовку списка или карты 
+// в зависимости от количества вернувшихся стран еще не придумал,
+// поэтому подставляю в зен либо  renderCountryList либо renderCountryCard
 
-function renderCountryCard(name) {
-  const markup = countryCard(name);
+// Вариант для отрисовки списка стран
+
+function renderCountryList(names) {
+  const markup = names
+    .map(({ name, flags }) => {
+      return `<li>
+          <p> ${flags.svg}</p>
+          <p> ${name.official}</p>
+        </li>`;
+    })
+    .join("");
+  countryList.innerHTML = markup;
+}
+
+// Вариант для отрисовки карточки одной страны
+function renderCountryCard({name,capital,population,flags,languages}) {
+  const markup = countryCard({name,capital,population,flags,languages});
   cardContainer.innerHTML = markup;
 }
 
 function onFetchError() {
-  // Notiflix.Notify.failure('Oops, there is no country with that name');
-  alert('Oops, there is no country with that name')
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+  // alert('Oops, there is no country with that name')
 }
+
+
+
+// function renderCountryCard(names) {
+//   const markup = names.map(({name,capital,population,flags,languages}) => {
+//     return `<div class="card">
+//   <div class="card-img-top">
+//     <img src="${flags.svg}" alt="flag">
+//     <h2 class="card-title">${name.official}</h2>
+//   </div>
+//   <div class="card-body">
+//     <p class="card-text">Capital: ${capital}</p>
+//     <p class="card-text">Population: ${population}</p>
+
+//     <p class="card-text"><b>languages:</b>
+//     <ul class="list-group">
+      
+//       <li class="list-group-item">${languages}</li>
+    
+//     </ul > 
+//     </p >
+    
+//   </div>
+// </div>`
+//   }
+
+//   )
+// }
+
