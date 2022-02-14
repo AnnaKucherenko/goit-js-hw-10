a803361452c8e04f5cab2f346d435be6c8432f4d
AnnaKucherenko <annakucherenko31@gmail.com>
2022-02-14 16:16:39 +0200

    исправление бага, стили через ссs

--- src/index.js
+++ src/index.js
@@ -11,69 +11,79 @@ const refs = getRefs();
 const DEBOUNCE_DELAY = 300;
 
 refs.inputEl.addEventListener('input', debounce(onTextInput, DEBOUNCE_DELAY));
-// let input = '';
  
-function onTextInput(evt){
-  evt.preventDefault();
-  const input = evt.target.value.trim();
+function onTextInput(){
   if (refs.inputEl.value.trim() === '') {
     clearCountryList();
     clearCountryInfo();
     return
-  } 
+  }   
   fetchCountries(refs.inputEl.value.trim())
     .then(names => {
       const numberOfCountries = names.length;
       if (numberOfCountries >= 10) {
-          clearCountryList();
-          clearCountryInfo();
-          Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-      } else 
-      if (numberOfCountries>= 2) {
-          renderCountryList(names);
-          clearCountryInfo();
-      } else
-      if (numberOfCountries === 1) {
         clearCountryList();
-        renderCountryCard(names);
-      }
-      
+        clearCountryInfo();
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        
+      } else
+        if (numberOfCountries >= 2 && numberOfCountries <= 10) {
+          clearCountryInfo();
+          renderCountryList(names);
+           
+        } else
+          if (numberOfCountries === 1) {
+            clearCountryList();
+            renderCountryCard(names);
+            
+          }
     })
-    .catch(onFetchError);
-  }
+    .catch(error => {
+      onFetchError(error),
+      console.log(error)
+    });
+  
+  
+  // refs.cardTitle.style.margin = '0px';
+  // refs.cardTitle.style.marginLeft = '20px';
+  
+  
+
+}
   
 const clearCountryList = () => {
   refs.countryList.innerHTML = '';
+  
 }
 const clearCountryInfo = () => {
   refs.cardContainer.innerHTML = '';
+  
 }
 // Вариант для отрисовки списка стран
 
 function renderCountryList(name) {
   const markup = listOfRelevantCountries(name);
   refs.countryList.innerHTML = markup;
-  imgEl.classList.add(img - country);
 }
 
 // Вариант для отрисовки карточки одной страны
 function renderCountryCard(name) {
   const markup = countryCard(name);
   refs.cardContainer.innerHTML = markup;
-  refs.containerCardImg.style.display = 'flex';
-  refs.cardTitle.style.margin = '0px';
-  refs.cardTitle.style.marginLeft = '20px';
-  refs.imgEl.classList.add(img - country);
-  refs.listLanguages.style.display = 'flex';
-  refs.listLanguages.style.marginLeft = "10px"
-  refs.languages.style.display = 'flex';
-     
 }
 
 function onFetchError() {
   Notiflix.Notify.failure('Oops, there is no country with that name');
 }
 
+// refs.imgEl.style.marginTop = 'auto';
+// refs.imgEl.style.marginLeft = 'auto';
+// refs.containerCardImg.style.display = 'flex';
+// refs.cardTitle.style.margin = '0px';
+// refs.cardTitle.style.marginLeft = '20px';
+// refs.listLanguages.style.display = 'flex';
+// refs.listLanguages.style.marginLeft = "10px"
+// refs.languages.style.display = 'flex';
 
 
 
@@ -86,32 +96,4 @@ function onFetchError() {
 
 
 
-
-
-
-// function renderCountryCard(names) {
-//   const markup = names.map(({name,capital,population,flags,languages}) => {
-//     return `<div class="card">
-//   <div class="card-img-top">
-//     <img src="${flags.svg}" alt="flag">
-//     <h2 class="card-title">${name.official}</h2>
-//   </div>
-//   <div class="card-body">
-//     <p class="card-text">Capital: ${capital}</p>
-//     <p class="card-text">Population: ${population}</p>
-
-//     <p class="card-text"><b>languages:</b>
-//     <ul class="list-group">
-      
-//       <li class="list-group-item">${languages}</li>
-    
-//     </ul > 
-//     </p >
-    
-//   </div>
-// </div>`
-//   }
-
-//   )
-// }
 
